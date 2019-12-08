4700f96927c0e139cbf71e75d9f855247810e4e2
Cristian Chiric <cristian.chiric@gmail.com>
2019-12-08 23:05:59 +0200

    Schedule command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(SendClientList::class)->weekly();
+        $schedule->command(SendClientList::class, '--to-users')->weekly();
     }
 
     /**
