4f95846b97da7d6b080b1775f13baeeeaa4aeeef
Cristian Chiric <cristian.chiric@gmail.com>
2019-12-09 16:52:23 +0200

    Fix parameter

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(SendClientList::class)->weekly();
-        $schedule->command(SendClientList::class, '--to-users')->weekly();
+        $schedule->command(SendClientList::class, ['--to-users'])->weekly();
     }
 
     /**
